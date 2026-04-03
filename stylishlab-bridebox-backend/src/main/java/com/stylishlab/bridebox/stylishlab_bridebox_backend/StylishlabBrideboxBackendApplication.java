caea2f54b9fa0e3f3edf3803c773e510fe550cea
Chamika Gayashan <dmforceeg@gmail.com>
2026-04-04 00:48:57 +0530

    Initial commit

---
+++ stylishlab-bridebox-backend/src/main/java/com/stylishlab/bridebox/stylishlab_bridebox_backend/StylishlabBrideboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stylishlab.bridebox.stylishlab_bridebox_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylishlabBrideboxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylishlabBrideboxBackendApplication.class, args);
+	}
+
+}
---
+++ stylishlab-bridebox-backend/src/test/java/com/stylishlab/bridebox/stylishlab_bridebox_backend/StylishlabBrideboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylishlab.bridebox.stylishlab_bridebox_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylishlabBrideboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
